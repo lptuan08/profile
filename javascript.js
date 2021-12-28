902d5b8ac70a9572d0f4db81f87eaa90b3db9119
tuan <phamtuan897@gmail.com>
2021-12-28 23:37:21 +0700

    add full code cv

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// let color = 'red';
+
+// const obj = {
+//     color: 'blue',
+// };
+
+// function sayColor() {
+//     console.log(this.color);
+// }
+
+// sayColor(); // red
+
+// obj.sayColor = sayColor;
+// obj.sayColor(); // blue
