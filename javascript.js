d8318746b0ae2f9a903a77e50c89693d688b2226
tuan <phamtuan897@gmail.com>
2021-12-30 21:33:09 +0700

    update navmenu mobile

--- javascript.js
+++ javascript.js
@@ -1,14 +1,33 @@
-// let color = 'red';
+var prevScrollpos = window.pageYOffset;
+        window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("header").style.top = "0";
+            document.querySelector("#mySidepanel").style.top = "0";
+        } else {
+            document.getElementById("header").style.top = "-50px";
+            document.querySelector("#mySidepanel").style.top = "-50px";
+        }
+        prevScrollpos = currentScrollPos;
+        }
 
-// const obj = {
-//     color: 'blue',
-// };
 
-// function sayColor() {
-//     console.log(this.color);
-// }
+// =====================================
+function openNav(){
+    document.getElementById("panel").style.display = "block";
 
-// sayColor(); // red
+};
+function closeNav(){
+    document.getElementById("panel").style.display = "none";
+    
+}
+// click hide item menu
+const panel = document.querySelector("#panel");
+const menuItems = document.querySelectorAll(".box-menu-list li");
+for(let i = 0; i< menuItems.length; i++){
+    let menuItem = menuItems[i];
+    menuItem.onclick = function(){
+        setTimeout(()=>{panel.style.display="none"},500);
+    }
+}
 
-// obj.sayColor = sayColor;
-// obj.sayColor(); // blue
